c8142e2aae5561ab6be64e9117596a396f564083
Rene kling <rene.kling1992@gmail.com>
2019-06-14 21:00:21 +0200

    Added michael protection

--- src/test/Generator.gd
+++ src/test/Generator.gd
@@ -1,53 +1,79 @@
 extends Node2D
 
+const MAX_SECRET_ROOMS = 5
+const MIN_SIZE_X = 7
+const MIN_SIZE_Y = 7
+const MAX_SIZE_X = 10
+const MAX_SIZE_Y = 10
+const MAX_ROOMS = 50
 
-var grid : Array
-export var size_x : int = 7
-export var size_y : int = 7
-export var room_count : int = 12
-export var secret_room_count : int = 2
+var m_grid : Array
+var m_size_x : int = 7
+var m_size_y : int = 7
+var m_room_count : int = 12
+var m_secret_room_count : int = 2
+var m_placed_rooms : Array 
+var m_generation_running : bool = false
 
 enum Room_Type {
+	NONE = 0,
 	START = 1,
 	NORMAL = 2,
 	SECRET = 3	
 }
 
-var placed_rooms : Array 
-
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
 	randomize()
 	
 
-func _init_grid():
-	grid = []
-	placed_rooms = []
-	for x in range(size_x):
-		grid.append([])
-		for y in range(size_y):
-			grid[x].append(0)
-	if (size_x * size_y) < room_count:
-		room_count = size_x*size_y	
+func _init_grid() -> void:
+	m_grid = []
+	m_placed_rooms = []
+	if m_size_x < MIN_SIZE_X:
+		m_size_x = MIN_SIZE_X
+	if m_size_x > MAX_SIZE_X:
+		m_size_x = MAX_SIZE_X
+	$SizeXEdit.text = String(m_size_x)
+	if m_size_y < MIN_SIZE_Y:
+		m_size_y = MIN_SIZE_Y
+	if m_size_y > MAX_SIZE_Y:
+		m_size_y = MAX_SIZE_Y
+	$SizeYEdit.text = String(m_size_y)
+	for x in range(m_size_x):
+		m_grid.append([])
+		for y in range(m_size_y):
+			m_grid[x].append(Room_Type.NONE)
+	if m_secret_room_count > MAX_SECRET_ROOMS:
+		m_secret_room_count = MAX_SECRET_ROOMS	
+		$SecretRoomCountEdit.text = String(m_secret_room_count)
+	if MAX_ROOMS < m_room_count:
+		m_room_count = MAX_ROOMS
+		$RoomCountEdit.text = String(m_room_count)
 		
-func _print_grid():
+func _print_grid() -> void:
 	print("---------------------------------------")
 	$RichTextLabel.text = ""
-	for x in range(size_x):
-		print(grid[x])	
-		$RichTextLabel.text += "\n" + String(grid[x])
+	for x in range(m_size_x):
+		print(m_grid[x])	
+		$RichTextLabel.text += "\n" + String(m_grid[x])
 	print("---------------------------------------")	
 		
 		
-func _generate_levels():
+func _generate_levels() -> void:
 	_place_start()
 	# Add attached rooms
-	for i in room_count:
+	_place_normal_rooms()
+	# Add not attached rooms
+	_place_secret_rooms()
+
+func _place_normal_rooms() -> void:
+	for i in m_room_count:
 		var possible_attachments = []
 		var already_closed_rooms = []
 		var retries = 0
 		while possible_attachments.size() == 0:
-			var rand_placed_room = placed_rooms[(randi() % placed_rooms.size())]
+			var rand_placed_room = m_placed_rooms[(randi() % m_placed_rooms.size())]
 			if already_closed_rooms.find(rand_placed_room) == -1:
 				var found_attachments = _get_possible_attachements_for_room(rand_placed_room)
 				if found_attachments.size() > 2 || retries > 20:
@@ -57,14 +83,15 @@ func _generate_levels():
 				retries += 1
 		var new_pos = possible_attachments[randi()%possible_attachments.size()]
 		_add_room(new_pos, Room_Type.NORMAL)
-	# Add not attached rooms
+
+func _place_secret_rooms() -> void:
 	var secret_rooms_placed = 0
 	var tries = 0
-	while secret_rooms_placed < secret_room_count:
-		var coord = Vector2(randi()%size_x, randi()%size_y)
-		for room in placed_rooms:
+	while secret_rooms_placed < m_secret_room_count:
+		var coord = Vector2(randi()%m_size_x, randi()%m_size_y)
+		for room in m_placed_rooms:
 			if (room.x == coord.x || room.y == coord.y) && !(room.x == coord.x && room.y == coord.y):
-				if grid[coord.x][coord.y] == 0:
+				if m_grid[coord.x][coord.y] == 0:
 					if _get_neighbours_count(coord) == 0:
 						_add_room(coord, Room_Type.SECRET)
 						secret_rooms_placed += 1
@@ -75,8 +102,8 @@ func _generate_levels():
 
 
 func _add_room(pos : Vector2, type : int):
-	grid[pos.x][pos.y] = type
-	placed_rooms.append(pos)
+	m_grid[pos.x][pos.y] = type
+	m_placed_rooms.append(pos)
 	
 	
 func _get_neighbours_count(room) -> int:
@@ -87,8 +114,8 @@ func _get_neighbours_count(room) -> int:
 		Vector2(room.x , room.y -1),
 		Vector2(room.x , room.y + 1)]
 	for comb in possible_combinations:
-		if(comb.x >= 0 && comb.x < size_x && comb.y >= 0 && comb.y < size_y):
-			if grid[comb.x][comb.y] != 0:
+		if(comb.x >= 0 && comb.x < m_size_x && comb.y >= 0 && comb.y < m_size_y):
+			if m_grid[comb.x][comb.y] != 0:
 				neighbours += 1
 	return neighbours
 	
@@ -100,15 +127,15 @@ func _get_possible_attachements_for_room(room) -> Array:
 		Vector2(room.x , room.y -1),
 		Vector2(room.x , room.y + 1)]
 	for comb in possible_combinations:
-		if(comb.x > 0 && comb.x < size_x && comb.y > 0 && comb.y < size_y):
-			if grid[comb.x][comb.y] == 0:
+		if(comb.x > 0 && comb.x < m_size_x && comb.y > 0 && comb.y < m_size_y):
+			if m_grid[comb.x][comb.y] == 0:
 				possible_attachments.append(comb)
 	return possible_attachments
 	
 
 	
-func _place_start():
-	var place = Vector2(randi()%size_x, randi()%size_y)
+func _place_start() -> void:
+	var place = Vector2(randi()%m_size_x, randi()%m_size_y)
 	_add_room(place, Room_Type.START)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -116,13 +143,16 @@ func _place_start():
 #	pass
 
 
-func _on_Button_pressed():
-	size_x = int($SizeXEdit.text)
-	size_y = int($SizeYEdit.text)
-	room_count = int($RoomCountEdit.text)
-	secret_room_count = int($SecretRoomCountEdit.text)
-	_init_grid()
-	_generate_levels()
-	_print_grid()
+func _on_Button_pressed() -> void:
+	if !m_generation_running:
+		m_generation_running = true
+		m_size_x = int($SizeXEdit.text)
+		m_size_y = int($SizeYEdit.text)
+		m_room_count = int($RoomCountEdit.text)
+		m_secret_room_count = int($SecretRoomCountEdit.text)
+		_init_grid()
+		_generate_levels()
+		_print_grid()
+		m_generation_running = false
 
 
