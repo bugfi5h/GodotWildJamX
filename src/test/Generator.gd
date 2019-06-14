ff638343bd1805902df97651b582b402f21f5e85
R.K <rene.kling@agentilo.com>
2019-06-14 15:42:06 +0200

    Added secret room generation

--- src/test/Generator.gd
+++ src/test/Generator.gd
@@ -5,6 +5,13 @@ var grid : Array
 export var size_x : int = 7
 export var size_y : int = 7
 export var room_count : int = 12
+export var secret_room_count : int = 2
+
+enum Room_Type {
+	START = 1,
+	NORMAL = 2,
+	SECRET = 3	
+}
 
 var placed_rooms : Array 
 
@@ -34,6 +41,7 @@ func _print_grid():
 		
 func _generate_levels():
 	_place_start()
+	# Add attached rooms
 	for i in room_count:
 		var possible_attachments = []
 		var already_closed_rooms = []
@@ -48,22 +56,41 @@ func _generate_levels():
 					already_closed_rooms.append(rand_placed_room)
 				retries += 1
 		var new_pos = possible_attachments[randi()%possible_attachments.size()]
-		_add_room(new_pos)
+		_add_room(new_pos, Room_Type.NORMAL)
+	# Add not attached rooms
+	var secret_rooms_placed = 0
+	var tries = 0
+	while secret_rooms_placed < secret_room_count:
+		var coord = Vector2(randi()%size_x, randi()%size_y)
+		for room in placed_rooms:
+			if (room.x == coord.x || room.y == coord.y) && !(room.x == coord.x && room.y == coord.y):
+				if grid[coord.x][coord.y] == 0:
+					if _get_neighbours_count(coord) == 0:
+						_add_room(coord, Room_Type.SECRET)
+						secret_rooms_placed += 1
+						break;
+		tries += 1
+		if tries >= 100:
+			break;
 
 
-func _add_room(pos : Vector2):
-	grid[pos.x][pos.y] = 1
+func _add_room(pos : Vector2, type : int):
+	grid[pos.x][pos.y] = type
 	placed_rooms.append(pos)
-	_print_grid()
-	#_wait(1)
 	
-func _wait(time_in_seconds: int):
-	var t = $Timer
-	t.wait_time = time_in_seconds
-	t.one_shot = true
-	t.start()
-	yield(t, "timeout")
 	
+func _get_neighbours_count(room) -> int:
+	var neighbours = 0
+	var possible_combinations = [ 
+		Vector2(room.x -1 , room.y),
+		Vector2(room.x +1 , room.y),
+		Vector2(room.x , room.y -1),
+		Vector2(room.x , room.y + 1)]
+	for comb in possible_combinations:
+		if(comb.x >= 0 && comb.x < size_x && comb.y >= 0 && comb.y < size_y):
+			if grid[comb.x][comb.y] != 0:
+				neighbours += 1
+	return neighbours
 	
 func _get_possible_attachements_for_room(room) -> Array:
 	var possible_attachments = []
@@ -82,7 +109,7 @@ func _get_possible_attachements_for_room(room) -> Array:
 	
 func _place_start():
 	var place = Vector2(randi()%size_x, randi()%size_y)
-	_add_room(place)
+	_add_room(place, Room_Type.START)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -93,7 +120,9 @@ func _on_Button_pressed():
 	size_x = int($SizeXEdit.text)
 	size_y = int($SizeYEdit.text)
 	room_count = int($RoomCountEdit.text)
+	secret_room_count = int($SecretRoomCountEdit.text)
 	_init_grid()
 	_generate_levels()
+	_print_grid()
 
 
