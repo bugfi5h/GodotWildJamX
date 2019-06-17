d5e05d7482224817451f20d41356f3522408aba1
Sam Acker <sam.acker@agentilo.com>
2019-06-17 12:17:01 +0200

    added glorbs

--- src/globals/Helper.gd
+++ src/globals/Helper.gd
@@ -6,3 +6,15 @@ func get_player() -> Node2D:
 	if players.size() > 0:
 		player = players[0]
 	return player
+	
+func get_direction(from : Vector2, to : Vector2) -> Vector2:
+	var direction = Vector2(to.x - from.x, to.y - from.y)
+	return direction.normalized()
+	
+func limit(minimum : float, maximum : float, value : float) -> float:
+	if(value < minimum):
+		value = minimum
+	elif(value > maximum):
+		value = maximum
+		
+	return value
