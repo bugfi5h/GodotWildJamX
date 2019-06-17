57289f019f1c8478182d1784c0d7f24c6c152629
Sam Acker <sam.acker@agentilo.com>
2019-06-17 15:42:46 +0200

    fixed placing of deco tiles

--- src/map/rooms/RandomRoom.gd
+++ src/map/rooms/RandomRoom.gd
@@ -12,8 +12,8 @@ func _ready():
 
 func generate_deco(amount : int) -> void:
 	for i in range(amount):
-		var x = (randi() % 30)
-		var y = (randi() % 16)
+		var x = (randi() % 28 + 1)
+		var y = (randi() % 14 + 1)
 		$Deco.set_cell(x, y, 5)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
