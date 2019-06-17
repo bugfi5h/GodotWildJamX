7752f1fcb797aa2553287a80385a4d63177044c5
Rene kling <rene.kling1992@gmail.com>
2019-06-17 20:42:06 +0200

    Added the g to settings...

--- src/ui/Settings.gd
+++ src/ui/Settings.gd
@@ -6,10 +6,10 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$MarginContainer/VBoxContainer/Musik/NinePatchRect/Volume/MusikSlider.value = GameSettins.music_volume
-	$MarginContainer/VBoxContainer/Sound/NinePatchRect/Volume/SoundSlider.value = GameSettins.sound_volume
-	$MarginContainer/VBoxContainer/Musik/BtnMusic.pressed = !GameSettins.music_enabled
-	$MarginContainer/VBoxContainer/Sound/BtnSound.pressed = !GameSettins.sound_enabled 
+	$MarginContainer/VBoxContainer/Musik/NinePatchRect/Volume/MusikSlider.value = GameSettings.music_volume
+	$MarginContainer/VBoxContainer/Sound/NinePatchRect/Volume/SoundSlider.value = GameSettings.sound_volume
+	$MarginContainer/VBoxContainer/Musik/BtnMusic.pressed = !GameSettings.music_enabled
+	$MarginContainer/VBoxContainer/Sound/BtnSound.pressed = !GameSettings.sound_enabled 
 
 func _on_BtnBack_pressed():
 	SceneLoader.goto_menu_scene("res://ui/MainMenue.tscn")
@@ -17,14 +17,14 @@ func _on_BtnBack_pressed():
 
 func _on_SoundSlider_value_changed(value):
 	$MarginContainer/VBoxContainer/Sound/NinePatchRect/Volume.value = value
-	GameSettins.set_sound_volume(value)
+	GameSettings.set_sound_volume(value)
 	
 func _on_MusikSlider_value_changed(value):
 	$MarginContainer/VBoxContainer/Musik/NinePatchRect/Volume.value = value
-	GameSettins.set_music_volume(value)
+	GameSettings.set_music_volume(value)
 
 func _on_BtnMusic_toggled(button_pressed):
-	GameSettins.set_music_enabled(!button_pressed)
+	GameSettings.set_music_enabled(!button_pressed)
 
 func _on_BtnSound_toggled(button_pressed):
-	GameSettins.set_sound_enabled(!button_pressed)
+	GameSettings.set_sound_enabled(!button_pressed)
