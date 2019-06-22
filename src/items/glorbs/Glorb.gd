ed1e03f9dbf633aa37592da05ac31ce6f00f9493
Valinor <sam.acker.business@gmail.com>
2019-06-23 00:39:23 +0200

    improved movement of glorbs

--- src/items/glorbs/Glorb.gd
+++ src/items/glorbs/Glorb.gd
@@ -1,7 +1,7 @@
 extends Area2D
 class_name Glorb
 
-export var speed : float = 1;
+export var speed : float = 2;
 export var glitch_amount : float = 10;
 
 var m_player : Node2D;
@@ -14,8 +14,8 @@ func _ready():
 func _physics_process(delta):
 	var direction = Helper.get_direction(global_position, m_player.global_position)
 	var distance = global_position.distance_to(m_player.global_position)
-	var speed_multiplier = (50 / pow(Helper.limit(5, 200, abs(distance)), 2)) * 100
-	speed_multiplier = Helper.limit(1, 10, speed_multiplier)
+	var speed_multiplier = (50 / pow(Helper.limit(0, 200, abs(distance)), 2)) * 100
+	speed_multiplier = Helper.limit(0, 10, speed_multiplier)
 	translate(direction * speed * speed_multiplier * delta)
 
 func _on_Glorb_body_entered(body):
